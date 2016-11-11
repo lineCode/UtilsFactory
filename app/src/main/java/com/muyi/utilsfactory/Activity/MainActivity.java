9354e01de57fb0f0a39606542e22442ee6ef526d
Yo Mi <Yo Mi>
2016-11-11 11:34:04 +0800

    Initial commit

---
+++ app/src/main/java/com/muyi/utilsfactory/Activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.muyi.utilsfactory.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.muyi.utilsfactory.R;
+
+/**
+ * Created by yoyid on 2016/11/11.
+ *
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/muyi/utilsfactory/UtilsFactoryApplication.java
@@ -0,0 +1,20 @@
+package com.muyi.utilsfactory;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by yoyid on 2016/11/11.
+ *
+ */
+
+public class UtilsFactoryApplication extends Application {
+
+    public Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = this;
+    }
+}
